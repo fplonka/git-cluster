149614432227f049be891169b059635d4bf7dbfb
Filip Płonka <github@1f389.com>
2024-03-31 14:48:17 +0200

    fix distance matrix data source path

--- metal/src/MetalSPE.cpp
+++ metal/src/MetalSPE.cpp
@@ -16,12 +16,14 @@
 MetalSPE::MetalSPE(std::string distance_matrix_filename)
     : m_params_filename(distance_matrix_filename) {}
 
-void MetalSPE::init_with_device(MTL::Device* device) {
+void MetalSPE::init_with_device(MTL::Device *device)
+{
     m_device = device;
-    NS::Error* error;
+    NS::Error *error;
     auto default_library = m_device->newDefaultLibrary();
 
-    if (!default_library) {
+    if (!default_library)
+    {
         std::cerr << "Failed to load default library.";
         std::exit(-1);
     }
@@ -32,7 +34,8 @@ void MetalSPE::init_with_device(MTL::Device* device) {
         NS::String::string("update_coords_bfloat", NS::ASCIIStringEncoding);
     auto spe_function = default_library->newFunction(function_name);
 
-    if (!spe_function) {
+    if (!spe_function)
+    {
         std::cerr << "Failed to find the update coords function.";
     }
 
@@ -41,39 +44,42 @@ void MetalSPE::init_with_device(MTL::Device* device) {
     m_command_queue = m_device->newCommandQueue();
 };
 
-bool isHalfNaN(uint16_t value) {
+bool isHalfNaN(uint16_t value)
+{
     // Mask for the exponent (5 bits set to 1, shifted left 10 positions)
-    const uint16_t exponentMask = 0x7C00;  // 0b0111110000000000
+    const uint16_t exponentMask = 0x7C00; // 0b0111110000000000
 
     // Mask for the fraction (10 bits set to 1)
-    const uint16_t fractionMask = 0x03FF;  // 0b0000001111111111
+    const uint16_t fractionMask = 0x03FF; // 0b0000001111111111
 
     // Check for NaN: exponent is all 1s and fraction is non-zero
     return (value & exponentMask) == exponentMask &&
            (value & fractionMask) != 0;
 }
 
-void MetalSPE::prepare_data() {
+void MetalSPE::prepare_data()
+{
     // Read run data and params from file
     std::cout << "Reading matrix data in C++ subprocess...\n";
     std::cout.flush();
     std::ifstream file(m_params_filename, std::ios::binary);
 
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         throw std::runtime_error("Unable to open dist matrix data file");
     }
 
     RunParams params{};
-    file.read(reinterpret_cast<char*>(&params.num_iterations),
+    file.read(reinterpret_cast<char *>(&params.num_iterations),
               sizeof(params.num_iterations));
-    file.read(reinterpret_cast<char*>(&params.initial_lr),
+    file.read(reinterpret_cast<char *>(&params.initial_lr),
               sizeof(params.initial_lr));
-    file.read(reinterpret_cast<char*>(&params.final_lr),
+    file.read(reinterpret_cast<char *>(&params.final_lr),
               sizeof(params.final_lr));
     m_params = params;
 
     int N;
-    file.read(reinterpret_cast<char*>(&N), sizeof(N));
+    file.read(reinterpret_cast<char *>(&N), sizeof(N));
     m_N = N;
 
     // Init with random coords
@@ -84,18 +90,21 @@ void MetalSPE::prepare_data() {
     size_t dist_matrix_size = m_N * m_N * sizeof(uint16_t);
     m_dist_matrix_buffer =
         m_device->newBuffer(dist_matrix_size, MTL::ResourceStorageModeShared);
-    if (!m_dist_matrix_buffer) {
+    if (!m_dist_matrix_buffer)
+    {
         std::cout << "couldn't make buffer\n...";
     }
-    file.read(reinterpret_cast<char*>(m_dist_matrix_buffer->contents()),
+    file.read(reinterpret_cast<char *>(m_dist_matrix_buffer->contents()),
               dist_matrix_size);
 
     // TEST READ
-    const uint16_t* halfBuffer =
-        reinterpret_cast<const uint16_t*>(m_dist_matrix_buffer->contents());
+    const uint16_t *halfBuffer =
+        reinterpret_cast<const uint16_t *>(m_dist_matrix_buffer->contents());
 
-    for (size_t i = 0; i < m_N * m_N; ++i) {
-        if (isHalfNaN(halfBuffer[i])) {
+    for (size_t i = 0; i < m_N * m_N; ++i)
+    {
+        if (isHalfNaN(halfBuffer[i]))
+        {
             std::cout << "NaN found at index " << i << std::endl;
         }
     }
@@ -111,26 +120,29 @@ void MetalSPE::prepare_data() {
     // }
 }
 
-void MetalSPE::generate_random_float2_data(MTL::Buffer* buffer,
-                                           unsigned long num_elements) {
+void MetalSPE::generate_random_float2_data(MTL::Buffer *buffer,
+                                           unsigned long num_elements)
+{
     std::mt19937 rng(std::random_device{}());
     std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
 
-    float2* data_ptr = (float2*)buffer->contents();
-    for (unsigned long index = 0; index < num_elements; index++) {
+    float2 *data_ptr = (float2 *)buffer->contents();
+    for (unsigned long index = 0; index < num_elements; index++)
+    {
         data_ptr[index] = float2(dist(rng), dist(rng));
     }
 }
 
-void MetalSPE::do_spe_loop() {
+void MetalSPE::do_spe_loop()
+{
     auto start = std::chrono::high_resolution_clock::now();
     float initial_lr = m_params.initial_lr;
     float final_lr = m_params.final_lr;
     uint num_iters = m_params.num_iterations;
 
-    std::random_device rd;   // obtain a random number from hardware
-    std::mt19937 gen(rd());  // seed the generator
-    std::uniform_int_distribution<> distr(0, m_N);  // define the range
+    std::random_device rd;                         // obtain a random number from hardware
+    std::mt19937 gen(rd());                        // seed the generator
+    std::uniform_int_distribution<> distr(0, m_N); // define the range
 
     const uint batch_size = 5000;
     uint batches_committed = 0;
@@ -139,11 +151,13 @@ void MetalSPE::do_spe_loop() {
     std::cerr << std::fixed << std::setprecision(2);
 
     std::cerr << "\n";
-    MTL::CommandBuffer* command_buffer = nullptr;
-    while (batches_committed < num_iters) {
+    MTL::CommandBuffer *command_buffer = nullptr;
+    while (batches_committed < num_iters)
+    {
         command_buffer = m_command_queue->commandBuffer();
         for (int itr_idx = 0;
-             itr_idx < batch_size && batches_committed < num_iters; ++itr_idx) {
+             itr_idx < batch_size && batches_committed < num_iters; ++itr_idx)
+        {
             float lr =
                 final_lr + 0.5f * (initial_lr - final_lr) *
                                (1.f + cos(M_PI * ((float)batches_committed) /
@@ -154,7 +168,7 @@ void MetalSPE::do_spe_loop() {
             //                             float(batches_committed);
             uint pivot_idx = distr(gen);
 
-            MTL::ComputeCommandEncoder* compute_encoder =
+            MTL::ComputeCommandEncoder *compute_encoder =
                 command_buffer->computeCommandEncoder();
             encode_spe_command(compute_encoder, pivot_idx, lr);
 
@@ -181,8 +195,9 @@ void MetalSPE::do_spe_loop() {
     // std::cout.flush();
 }
 
-void MetalSPE::encode_spe_command(MTL::ComputeCommandEncoder* compute_encoder,
-                                  uint pivot_idx, float learning_rate) {
+void MetalSPE::encode_spe_command(MTL::ComputeCommandEncoder *compute_encoder,
+                                  uint pivot_idx, float learning_rate)
+{
     compute_encoder->setComputePipelineState(m_spe_function_pso);
 
     compute_encoder->setBuffer(m_coords_buffer, 0, 0);
@@ -197,7 +212,8 @@ void MetalSPE::encode_spe_command(MTL::ComputeCommandEncoder* compute_encoder,
     NS::UInteger thread_group_size_ =
         m_spe_function_pso->maxTotalThreadsPerThreadgroup();
 
-    if (thread_group_size_ > m_N) {
+    if (thread_group_size_ > m_N)
+    {
         thread_group_size_ = m_N;
     }
 
@@ -206,11 +222,13 @@ void MetalSPE::encode_spe_command(MTL::ComputeCommandEncoder* compute_encoder,
     compute_encoder->dispatchThreads(grid_size, thread_group_size);
 }
 
-void MetalSPE::write_results() {
-    float2* bufferPointer = static_cast<float2*>(m_coords_buffer->contents());
+void MetalSPE::write_results()
+{
+    float2 *bufferPointer = static_cast<float2 *>(m_coords_buffer->contents());
 
     std::ofstream outputFile("metal/embeddings.txt");
-    for (int i = 0; i < m_N; ++i) {
+    for (int i = 0; i < m_N; ++i)
+    {
         outputFile << bufferPointer[i].x << ", " << bufferPointer[i].y
                    << std::endl;
     }
--- metal/src/main.cpp
+++ metal/src/main.cpp
@@ -10,14 +10,19 @@
 #include <Foundation/Foundation.hpp>
 #include <Metal/Metal.hpp>
 #include <iostream>
+#include <filesystem>
 
 #include "MetalSPE.hpp"
 
-void testSPE() {
-    NS::AutoreleasePool* p_pool = NS::AutoreleasePool::alloc()->init();
-    MTL::Device* device = MTL::CreateSystemDefaultDevice();
-    MetalSPE* spe =
-        new MetalSPE("/Users/filip/Code/python/git-cluster/dist_matrix_data");
+void testSPE()
+{
+    NS::AutoreleasePool *p_pool = NS::AutoreleasePool::alloc()->init();
+    MTL::Device *device = MTL::CreateSystemDefaultDevice();
+    // TODO: JFC
+    MetalSPE *spe =
+        new MetalSPE(std::filesystem::current_path().string() + "/dist_matrix_data");
+
+    std::filesystem::path cwd = std::filesystem::current_path();
 
     spe->init_with_device(device);
     spe->prepare_data();
@@ -28,7 +33,8 @@ void testSPE() {
     p_pool->release();
 }
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[])
+{
     // testAdder();
     testSPE();
 
